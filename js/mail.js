97b655e5667d0fe561ce41f4afd563a1df05d650
bobrovkirill <bobrovkirill11@gmail.com>
2020-09-23 13:54:01 +0300

    add contact form

---
+++ js/mail.js
@@ -0,0 +1,27 @@
+function validateForm() {
+document.getElementById('status').innerHTML = "Sending...";
+formData = {
+    'name'     : $('input[name=name]').val(),
+    'email'    : $('input[name=email]').val(),
+    'tel'      : $('input[name=tel]').val(),
+    'message'  : $('textarea[name=message]').val()
+};
+
+
+$.ajax({
+url : "../mail.php",
+type: "POST",
+data : formData,
+success: function(data, textStatus, jqXHR)
+{
+
+    $('#status').text(data.message);
+    if (data.code) //If mail was sent successfully, reset the form.
+    $('#contact-form').closest('form').find("input[type=text], textarea").val("");
+},
+error: function (jqXHR, textStatus, errorThrown)
+{
+    $('#status').text(jqXHR);
+}
+});
+}
